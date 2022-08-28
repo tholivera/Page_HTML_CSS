0c443637326b50c6d8384c7f1f9a32ab0b52c1f7
tholivera <emaildathaisoliveira.silva@gmail.com>
2022-08-28 20:34:15 -0300

    Adicionando index.js

---
+++ pages/Contato/index.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let formulario = document.querySelector("#formulario");
+    let nome = formulario.nome.value;
+    let email = formulario.email.value;
+    let mensagem = formulario.mensagem.value;
+    
+    body = {
+        nome: nome,
+        email: email,
+        mensagem: mensagem
+    }
+
+    let request = new XMLHttpRequest(); //objeto que permite fazer requisições http
+    request.open("POST", "http://127.0.0.1:8080/mensagens", true); //conectar front com back
+    request.setRequestHeader("Content-type", "application/json"); //informando tipo de arquivo que será recebido
+    request.send(JSON.stringify(body)); //enviar requisição post já convertendo os dados para json
+
+    request.onload = function () {
+        console.log(this.responseText)
+    } // imprime no console os dados enviados
+
+
+})
