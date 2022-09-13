b035a98ba0b792c56f0c4650dc5caaa736ef4aa4
tholivera <emaildathaisoliveira.silva@gmail.com>
2022-09-13 19:32:44 -0300

    Adicionando API JS

---
+++ api/index.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const cors = require('cors')
+const server = express()
+const mensagens = []
+const cadastrar = []
+
+server.use(express.json())
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    server.use(cors())
+    next()
+})
+
+//cadastro de novos usuários
+server.get("/ver-cadastro", (req, res) => {
+    return res.json(cadastrar)
+})
+
+server.post("/cadastrar", (req, res) => {
+
+    const { nome, email, senha } = req.body
+
+    const cadastro = {
+        "nome": nome,
+        "email": email,
+        "senha": senha
+    }
+
+    cadastrar.push(cadastro)
+
+    return res.json(cadastrar)
+})
+
+server.put("/atualizar-cadastro/:index", (req, res) => {
+    const { index } = req.params;
+    const { nome, email, senha } = req.body
+
+    const cadastro = {
+        "nome": nome,
+        "email": email,
+        "senha": senha
+    }
+
+    cadastrar[index] = cadastro;
+
+    return res.json(cadastrar);
+})
+
+server.delete("/apagar-cadastro/:index", (req, res) => {
+    const { index } = req.params;
+
+    cadastrar.splice(index, 1);
+
+    return res.send("Usuário deletado com sucesso");
+})
+
+//cadastro de novas mensagens
+
+server.get("/ver-mensagens", (req, res) => {
+    return res.json(mensagens)
+})
+
+server.post("/mensagens", (req, res) => {
+
+    const { nome, email, mensagem } = req.body
+
+    const novaMensagem = {
+        "nome": nome,
+        "email": email,
+        "mensagem": mensagem
+    }
+
+    mensagens.push(novaMensagem)
+
+    return res.json(mensagens)
+})
+
+server.put("/atualizar-mensagem/:index", (req, res) => {
+    const { index } = req.params;
+    const { nome, email, mensagem } = req.body
+
+    const novaMensagem = {
+        "nome": nome,
+        "email": email,
+        "mensagem": mensagem
+    }
+
+    mensagens[index] = novaMensagem;
+
+    return res.json(mensagens);
+})
+
+server.delete("/apagar-mensagem/:index", (req, res) => {
+    const { index } = req.params;
+
+    mensagens.splice(index, 1);
+
+    return res.send("Mensagem deletada com sucesso");
+})
+
+server.listen(3000);
